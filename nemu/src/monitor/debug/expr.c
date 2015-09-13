46679f5c7dc1b5a8dc1d5e5f3d18d332508338b2
tracer-ics2015 <tracer@njuics.org>
2015-09-13 08:31:52 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  08:31:52 up 23:17,  3 users,  load average: 0.01, 0.02, 0.05 1ed22baa71b63998a3c6f38ffc3a518f9f5051c3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -29,7 +29,7 @@ static struct rule {
     {"\\-", '-'},                   // substract
     {"\\*", '*'},                   // multiply
     {"\\/", '/'},                   // divide
-    {"d+", Num},                    // number
+    {"\\d+", Num},                    // number
 	{"==", EQ}					    // equal
 };
 
@@ -74,7 +74,8 @@ static bool make_token(char *e) {
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
 			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-				char *substr_start = e + position;
+	
+                char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
 				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
