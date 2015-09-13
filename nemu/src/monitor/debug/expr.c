dc7aad4d101d8d42056939ad885564836f5c700f
tracer-ics2015 <tracer@njuics.org>
2015-09-14 01:57:56 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:57:56 up 1 day, 16:43,  3 users,  load average: 0.00, 0.01, 0.05 b520555f97d24848f73f9a9a1e4305295c9b49b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -143,7 +143,8 @@ uint32_t expr(char *e, bool *success) {
 //recurssion to evaluate the expr
 uint32_t eval(uint32_t p, uint32_t q) {
     uint32_t k=0;
-    printf("k is %d!\n", k-1);
+    k--;
+    printf("k is %d!\n", k);
     if ( p > q){
         printf("bad eval!\n");
         assert(0);
