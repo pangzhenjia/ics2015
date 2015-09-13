5c57e1f086443d4c7b9fe1403e90272264ec995d
tracer-ics2015 <tracer@njuics.org>
2015-09-13 09:09:05 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  09:09:05 up 23:54,  3 users,  load average: 0.01, 0.04, 0.05 ce83425d15ff1a0d895ee72366ab423dd664c6b3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -126,7 +126,7 @@ int expr(char *e, bool *success) {
 
 int eval(uint32_t p, uint32_t q) {
     printf("p is %d, q is %d!\n", p, q);
-    printf("Num token type is %d!\n", tokens[0].type);
+    printf("Num token type is %d!\n", tokens[1].type);
     if ( p > q){
         printf("bad eval!\n");
         assert(0);
