dbc5114afed4ba81e5435b3a9086c9eeba9a6e38
tracer-ics2015 <tracer@njuics.org>
2015-09-15 15:27:35 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:27:35 up 3 days,  6:13,  5 users,  load average: 0.00, 0.02, 0.05 c9117d07538219fd269c42fee72228f2f8d5f0ee

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -33,6 +33,9 @@ WP* new_wp(){
             re -> next = NULL;
         }
         head = re;       // make the using wp in head 
+        if ( head -> next == NULL){
+            printf("head only have one!");
+        }
         return re;
     }
     else{
