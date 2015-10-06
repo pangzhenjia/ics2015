f27fe37bc5b584fd90d8d43800e6c91cb881a83e
tracer-ics2015 <tracer@njuics.org>
2015-10-06 15:36:52 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  15:36:52 up 1 day, 17:40,  8 users,  load average: 1.08, 1.03, 0.97 b7b1f7f45c43de6f6acd85f5d3559d301fe8e032

--- nemu/src/cpu/exec/control/push.c
+++ nemu/src/cpu/exec/control/push.c
@@ -7,20 +7,15 @@ int push_i2rm_l(swaddr_t eip);
 
 make_helper_v(push_i2rm);
 static void do_push_l_r();
-static int decode_i_r(swaddr_t eip);
 
 int push_i2rm_w(swaddr_t eip){
     return 1;
 }
 
 int push_i2rm_l(swaddr_t eip){
-    return idex(eip, decode_i_r, do_push_l_r);
+    return idex(eip, decode_r_l, do_push_l_r);
 }
 
-static int decode_i_r(swaddr_t eip){
-   decode_r_l(eip); 
-   return 0;
-}
 
 static void do_push_l_r(){
     int val = op_src -> val;
