8627331740c0ab1528ed1694c7c6a812749a54ac
tracer-ics2015 <tracer@njuics.org>
2015-10-06 13:03:55 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  13:03:55 up 1 day, 15:07,  5 users,  load average: 0.00, 0.01, 0.05 56238ee24b0f2bbcc61c05b24dec1a74842966de

--- nemu/src/cpu/exec/arith/sub.c
+++ nemu/src/cpu/exec/arith/sub.c
@@ -51,9 +51,9 @@ static void do_sub_l(){
     int val2 = val4 ^ (val4 >> 2);
     int val1 = val2 ^ (val2 >> 1);
     cpu.pf = !val1;
-    printf("be_sub is 0x%x!\n", be_sub);
-    printf("sub is 0x%x!\n", sub);
-    printf("result is 0x%x!\n", result);
-    printf("esp is 0x%x!\n", cpu.esp);
+    //printf("be_sub is 0x%x!\n", be_sub);
+    //printf("sub is 0x%x!\n", sub);
+    //printf("result is 0x%x!\n", result);
+    //printf("esp is 0x%x!\n", cpu.esp);
 }
 
