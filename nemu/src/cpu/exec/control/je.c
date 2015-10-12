79cbca3d94f138e3745b5a52272f71454359a564
tracer-ics2015 <tracer@njuics.org>
2015-10-13 04:29:28 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  04:29:28 up  4:52,  5 users,  load average: 0.00, 0.01, 0.03 abb1aef969ccfca6ef416e29e0f4a672c84f64f

--- nemu/src/cpu/exec/control/je.c
+++ nemu/src/cpu/exec/control/je.c
@@ -6,6 +6,7 @@ int je_rm2r_b(swaddr_t eip){
     int len = decode_rm_b(eip + 1);
     if (cpu.zf == 0){
         int val = op_src -> val;
+        printf("val is %d!\n", val);
         eip += val;
         printf("je: the next eip is 0x%x!\n", eip);
     }
