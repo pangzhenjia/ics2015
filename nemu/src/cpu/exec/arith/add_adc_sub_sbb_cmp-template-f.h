f184a4be3f56331a610e3f7b27ffe9bf13790996
tracer-ics2015 <tracer@njuics.org>
2015-10-14 02:53:11 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  02:53:11 up 1 day,  3:15,  9 users,  load average: 0.00, 0.01, 0.05 793e71596ea519ee344daf7b55fff113f2ecb46

--- nemu/src/cpu/exec/arith/add_adc_sub_sbb_cmp-template-f.h
+++ nemu/src/cpu/exec/arith/add_adc_sub_sbb_cmp-template-f.h
@@ -11,7 +11,7 @@ static void do_execute(){
     eflags_zspf(val);
     printf("val is 0x%x! CF is %d \n", val, cpu.cf);
     concat(eflags_ocf_, add_sub_flags)(val1, val2);
-#if instr_num == 1 || instr_num == 2
+#if instr_num == 1 || instr_num == 2 || instr_num == 3 || instr_num == 4
     printf("write: instr !\n ");
     OPERAND_W(op_dest, val);
 #endif
