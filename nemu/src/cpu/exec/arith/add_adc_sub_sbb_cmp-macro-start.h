133e1def619b0eb1d789d9f1d8b6246715d7137e
tracer-ics2015 <tracer@njuics.org>
2015-10-14 01:11:22 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  01:11:22 up 1 day,  1:34,  5 users,  load average: 0.00, 0.01, 0.05 72c0e405633307d551fb58dd3bef1e664773651

--- nemu/src/cpu/exec/arith/add_adc_sub_sbb_cmp-macro-start.h
+++ nemu/src/cpu/exec/arith/add_adc_sub_sbb_cmp-macro-start.h
@@ -1,25 +1,30 @@
 
 #if instr == adc
+
 #define CF cpu.cf
 #define add_sub_op +
 #define add_sub_flags add
 
 #elif instr == add
+
 #define CF 0
 #define add_sub_op +
 #define add_sub_flags add
 
 #elif instr == sub
+
 #define CF 0
 #define add_sub_op -
 #define add_sub_flags sub
 
 #elif instr == sbb 
+
 #define CF cpu.cf
 #define add_sub_op -
 #define add_sub_flags sub
 
 #else
+
 #define CF 0
 #define add_sub_op -
 #define add_sub_flags sub
