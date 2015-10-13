f4d95be9ca6076cf037bf1b2ac920b623ede4204
tracer-ics2015 <tracer@njuics.org>
2015-10-14 02:50:32 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  02:50:32 up 1 day,  3:13,  9 users,  load average: 0.05, 0.03, 0.05 88761f1729ff47930322c21fed4e0a61bf6b7c8

--- nemu/src/cpu/exec/arith/add_adc_sub_sbb_cmp-template-h.h
+++ nemu/src/cpu/exec/arith/add_adc_sub_sbb_cmp-template-h.h
@@ -1,4 +1,3 @@
-#include "add_adc_sub_sbb_cmp-macro-start.h"
 
 make_helper(concat(instr, _i2a_b));
 make_helper(concat(instr, _i2a_v));
@@ -10,4 +9,3 @@ make_helper(concat(instr, _r2rm_v));
 make_helper(concat(instr, _rm2r_b));
 make_helper(concat(instr, _rm2r_v));
 
-#include "add_adc_sub_sbb_cmp-macro-end.h"
