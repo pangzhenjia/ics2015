423e47c442d3693c95244c7536990492b33f7ed5
tracer-ics2015 <tracer@njuics.org>
2015-10-20 14:01:13 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  14:01:13 up  3:44,  6 users,  load average: 0.00, 0.01, 0.05 a0756cf295436ae3ba252e1d2a7e964832a9d48c

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -4,8 +4,8 @@ make_helper(concat(call_i_, SUFFIX)){
     reg_l(R_ESP) -= DATA_BYTE;
     int len = concat(decode_i_, SUFFIX)(eip + 1);
     MEM_W(reg_l(R_ESP), eip + len + 1);
-    printf("Yes, here!");
     int val = op_src -> val;
+    printf("val is %d!\n",val);
     return len + val + 1;
 }
 
