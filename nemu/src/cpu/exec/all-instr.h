90a865b8d65c5a37ea2b5a70963b12045a5764f0
tracer-ics2015 <tracer@njuics.org>
2015-10-20 12:34:22 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  12:34:22 up  2:17,  5 users,  load average: 0.00, 0.01, 0.05 7a56853b286a3718a542d49cd7e30e527eed0abb

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -37,6 +37,7 @@
 
 #include "string/rep.h"
 #include "string/movs.h"
+#include "string/stos.h"
 
 #include "misc/misc.h"
 
---
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -0,0 +1,10 @@
+#include "cpu/exec/template-start.h"
+
+int concat(stos_, SUFFIX)(swaddr_t eip){
+    DATA_TYPE val = REG(R_EAX);
+    MEM_W(reg_l(R_EDI), val);
+    reg_l(R_EDI) -= DATA_BYTE;
+    return 1;
+}
+
+#include "cpu/exec/template-end.h"
