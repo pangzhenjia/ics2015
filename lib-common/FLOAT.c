3c199299ccad87afa4993124118426629b867240
tracer-ics2015 <tracer@njuics.org>
2015-10-21 05:30:42 +0800

    >  run 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  05:30:42 up 19:13,  5 users,  load average: 0.00, 0.02, 0.05 dfbbb2866daf0af376554ffc010aa8dbf7f4f6

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -14,8 +14,41 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT f2F(float a) {
-    a = a * (1<<16);
-	return (FLOAT)a;
+    unsigned uf = (int)a & (~0);
+    unsigned exponent = uf << 1 >> 24;
+    unsigned fraction = uf << 9 >> 9;
+    unsigned sign = uf >> 31;
+    int new_exponent, val, shift;
+    if ( exponent == 0xff ){
+        return 0x80000000u;
+    }
+    else if(exponent == 0){ 
+        return 0;
+    }
+    else{
+        exponent = exponent - 127;
+        new_exponent = exponent;
+        if(new_exponent < 0){ // unsigned exp can not < 0
+            return 0;
+        }
+        else if(exponent < 24){
+            shift = 23 - exponent;
+            fraction = fraction >> shift;
+            val = ( 1 << exponent) | fraction;
+            if ( sign ){ val = -val; }
+            return val;
+        }
+        else if(exponent < 31){
+            shift = exponent -23;
+            fraction = fraction << shift;
+            val = ( 1 << exponent) | fraction;
+            if ( sign ){ val = -val; }
+            return val;
+        }
+        else{  // overflow
+            return 0x80000000;
+        }
+    }
 }
 
 FLOAT Fabs(FLOAT a) {
