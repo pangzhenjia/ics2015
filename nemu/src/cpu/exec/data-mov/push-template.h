d6db3c1086a57a465e3165ed3f4dfb072afd5e5c
tracer-ics2015 <tracer@njuics.org>
2015-10-20 14:42:57 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  14:42:57 up  4:26,  6 users,  load average: 0.00, 0.01, 0.05 211efeb7a6c047bc93aedbbbfe4e74d1c5a6fe

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -3,7 +3,6 @@
 #define instr push
 
 static void do_execute(){
-    printf("tell me!\n");
     DATA_TYPE val = op_src -> val;
     reg_l(R_ESP) -= DATA_BYTE;
     //printf("addr is 0x%x!\n", reg_l(R_ESP)); 
