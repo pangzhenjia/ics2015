8d883170043981fe850dfdd1cb488d5b72d4130d
tracer-ics2015 <tracer@njuics.org>
2015-10-20 16:21:30 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  16:21:30 up  6:04,  7 users,  load average: 0.00, 0.01, 0.05 b4c0fb5a5eaa8658c65e3b78ce6b8a38b7f26b33

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -3,7 +3,7 @@
 make_helper(concat(movsx_b_rm2r_, SUFFIX)) {
     int len = decode_rm2r_b(eip + 1);
     DATA_TYPE_S val = (DATA_TYPE_S)(op_src->val & 0xff);
-    printf("val is 0x%x!", op_src->val);
+    printf("val is 0x%x!", val);
     REG(op_dest->reg) = val;
     return len + 1;
 }
