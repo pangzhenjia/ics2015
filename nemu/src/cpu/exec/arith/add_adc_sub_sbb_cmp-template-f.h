72a963ad07f8281e5703f2fd6f82a909bce303b6
tracer-ics2015 <tracer@njuics.org>
2015-10-20 15:44:09 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  15:44:09 up  5:27,  7 users,  load average: 0.00, 0.09, 0.14 43f4e41dc575f17a6fb26efb688c3c55a73bb40e

--- nemu/src/cpu/exec/arith/add_adc_sub_sbb_cmp-template-f.h
+++ nemu/src/cpu/exec/arith/add_adc_sub_sbb_cmp-template-f.h
@@ -8,13 +8,14 @@ static void do_execute(){
     DATA_TYPE val1 = op_dest -> val;
     DATA_TYPE val2 = op_src -> val;
     DATA_TYPE val = val1 add_sub_op (val2 + CF);
-    eflags_zspf(val);
 #if instr_num == 1 || instr_num == 2 || instr_num == 3 || instr_num == 4
+    eflags_zspf(val);
     concat(eflags_ocf_, add_sub_flags)(val1, val2);
     OPERAND_W(op_dest, val);
 #endif
 
 #if instr_num == 5
+    eflags_zspf(-val);
     eflags_ocf_sub(val2, val1);
 #endif
     printf("val1 is %d, val2 is %d!\n", val1, val2);
