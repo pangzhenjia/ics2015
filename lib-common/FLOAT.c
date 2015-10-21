6f69e8863c95b3d15706a68e69e3a6c031b39fb9
tracer-ics2015 <tracer@njuics.org>
2015-10-21 11:34:26 +0800

    >  run 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  11:34:26 up 1 day,  1:17,  7 users,  load average: 0.80, 0.51, 0.28 5079262bb5ad3de9b6c26007c217cf8b890a2f69

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -8,12 +8,10 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-    FLOAT val1 = a & 0xffff0000;
-    val1 = val1 / b;
-    val1 = val1 << 16;
-    FLOAT val2 = a & 0xffff;
-    val2 = val2 << 15;
-    val2 = val2 / b;
+    FLOAT val = a & 0xffff0000;
+    FLOAT val1 = (val / b) << 16;
+    val = a & 0xffff;
+    FLOAT val2 = (val << 15) / b;
     val2 = val2 << 1;
     //int sign = val2 >> 31;
     //val2 = (val2 ^ sign) + (sign & 1);
