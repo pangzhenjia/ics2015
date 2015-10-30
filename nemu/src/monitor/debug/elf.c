89df930e9895aaeb068d163aee61c8e855add5f8
tracer-ics2015 <tracer@njuics.org>
2015-10-30 15:15:21 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  15:15:21 up  5:37,  4 users,  load average: 0.02, 0.03, 0.05 3ae873a845f81c7b4eac865fcd7c5d77b5ef80b

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -152,8 +152,8 @@ int reset_stack(){
     uint32_t ebp = swaddr_read(stack_frame.prev_ebp, 4);
     printf("ebp is 0x%08x\n", ebp);
     if(ebp == 0){ return 0; }
-    stack_frame.prev_ebp = ebp;
     stack_frame.ret_addr = swaddr_read(stack_frame.prev_ebp + 4, 4);
+    stack_frame.prev_ebp = ebp;
     if(swaddr_read(ebp, 4) == 0){ return 1; }
     int i;
     for(i = 0; i < 4; i++){
