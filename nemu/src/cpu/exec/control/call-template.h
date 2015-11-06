62c5ea2372365173000e2a66c947d1f0a9c106d7
tracer-ics2015 <tracer@njuics.org>
2015-11-07 02:11:00 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  02:11:00 up  5:40,  3 users,  load average: 0.03, 0.03, 0.05 7e97aedec3055e09719d097f87fbb3902df075db

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -14,7 +14,8 @@ make_helper(concat(call_rm_, SUFFIX)){
     int len = concat(decode_rm_, SUFFIX)(eip + 1);
     MEM_W(reg_l(R_ESP), eip + len + 1);
     DATA_TYPE val = (DATA_TYPE)op_src -> val;
-    return val - eip;
+    cpu.eip += val;
+    return 0;
 }
 
 
