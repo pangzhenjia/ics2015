ef7fe526dd1711471515f959932b093bcda5b6cf
tracer-ics2015 <tracer@njuics.org>
2015-11-07 01:34:35 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  01:34:35 up  5:04,  3 users,  load average: 0.05, 0.05, 0.05 2a1de4b9f569f0d91e1f493d3a390b53eba047b

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -64,7 +64,7 @@ void cpu_exec(volatile uint32_t n) {
 		 * instruction decode, and the actual execution. */
 		int instr_len = exec(cpu.eip);
         
-        printf("eip is %d\n",cpu.eip);
+        printf("eip is 0x%x\n",cpu.eip);
 
 		cpu.eip += instr_len;
 
