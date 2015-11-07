b75587b586cb7c19846cbd36551cc303d86da92d
tracer-ics2015 <tracer@njuics.org>
2015-11-07 08:59:52 +0800

    >  run 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  08:59:52 up 12:29,  3 users,  load average: 0.00, 0.01, 0.05 2a594bff6d87df284d2ea60c3787429e2a16d6dd

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -14,12 +14,14 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
     unsigned b_n = b_abs >> 16;
     unsigned b_p = b & 0xffff;
 
-
+    /* multiple by part */
     unsigned val1 = a_abs * b_n;
     unsigned val2 = a_n * b_p;
     unsigned val3 = (a * b);
     val3 >>= 16;
-    unsigned val = val1 + val2 + val3;
+
+    /* get val and get sign */
+    int val = val1 + val2 + val3;
     if ( (a >> 16) * (b >> 16) < 0){ val = -val;}
     return val;
 }
