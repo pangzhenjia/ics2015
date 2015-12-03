56cabd29d6c9c335d5c01b778618f275e72f9232
tracer-ics2015 <tracer@njuics.org>
2015-12-03 10:04:20 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  10:04:20 up  1:34,  3 users,  load average: 0.00, 0.01, 0.05 4dc734c89e185440312125f7f529495b2f98995

--- nemu/src/cpu/exec/data-mov/lgdt.c
+++ nemu/src/cpu/exec/data-mov/lgdt.c
@@ -1,12 +1,12 @@
 #include "cpu/exec/helper.h"
 
 int lgdt_rm2r_l(swaddr_t eip){
-    uint32_t addr = instr_fetch(eip+1, 4);
-    uint32_t val_limit = swaddr_read(addr, 2);
-    uint32_t val_base  = swaddr_read(addr+2, 4);
+    int len = decode_rm_w(eip + 1);
+    uint32_t val_limit = op_src->val;
+    uint32_t val_base  = swaddr_read(op_src->addr+2, 4);
     cpu._gdtr.limit = val_limit;
     cpu._gdtr.base  = val_base;
-    return 5;
+    return len+1;
 }
 
 int lgdt_rm2r_w(swaddr_t eip){
