4a89bf25f07d46c198bd3cfb21ded45c08afb481
tracer-ics2015 <tracer@njuics.org>
2015-12-08 13:32:57 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  13:32:57 up  4:30,  3 users,  load average: 0.05, 0.04, 0.05 ad3978fdbfb367fda56da37999989087d5638985

--- nemu/src/memory/segment.c
+++ nemu/src/memory/segment.c
@@ -35,10 +35,7 @@ uint32_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg){
 
     if(cpu.CR0.pe == 0){ return addr; }
 
-    uint32_t index = cpu.Sreg[sreg].index;
-
-    uint64_t base_limit = decode_gdt(index);
-    uint32_t seg_base = base_limit >> 32;
+    uint32_t seg_base = cpu.Sreg[sreg].base;
     printf("seg_base is 0x%x\n", seg_base);
 
     return addr + seg_base;
