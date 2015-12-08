ca7bf158f3affa451e7d6a0c4d389ffdc553f08f
tracer-ics2015 <tracer@njuics.org>
2015-12-08 22:03:58 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  22:03:58 up 8 min,  3 users,  load average: 0.00, 0.03, 0.05 50d4bd00c629d30c62dc8f47a6aa8a1edd341753

--- nemu/src/cpu/exec/control/ljmp.c
+++ nemu/src/cpu/exec/control/ljmp.c
@@ -1,11 +1,17 @@
 #include "cpu/exec/helper.h"
 
+extern uint64_t decode_gdt(uint32_t index);
+
 int ljmp(swaddr_t eip){
     uint32_t addr = instr_fetch(eip+1, 4);
     cpu.eip = addr;
 
     uint16_t cs = instr_fetch(eip+5, 2);
     cpu.Sreg[SR_CS].val = cs;
+    
+    uint32_t limit = decode_gdt(cpu.Sreg[SR_CS].index);
+    uint32_t base  = decode_gdt(cpu.Sreg[SR_CS].index);
+    printf("limit is 0x%x, base is 0x%x\n", limit, base);
 
     return 0;
 }
