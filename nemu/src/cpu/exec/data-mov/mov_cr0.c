e703ef9092dc036b7e9a61182af5e9f90308674e
tracer-ics2015 <tracer@njuics.org>
2015-12-08 09:53:52 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  09:53:52 up 51 min,  3 users,  load average: 0.00, 0.03, 0.05 cbf5f77e85c49b2aefd9ef75db462c104a621879

--- nemu/src/cpu/exec/data-mov/mov_cr0.c
+++ nemu/src/cpu/exec/data-mov/mov_cr0.c
@@ -1,15 +1,16 @@
 #include "cpu/exec/helper.h"
 
-int mov_to_cr0(swaddr_t eip){
+int mov_r2cr0(swaddr_t eip){
     int len = decode_rm_l(eip + 1);
     cpu.cr0 = op_src -> val;
+	print_asm_template2();
     return len+1;
 }
 
 #define DATA_BYTE 4
 #include "cpu/exec/template-start.h"
 
-int mov_from_cr0(swaddr_t eip){
+int mov_cr02r(swaddr_t eip){
     int len = decode_rm_l(eip + 1);
     OPERAND_W(op_src, cpu.cr0);
     return len+1;
