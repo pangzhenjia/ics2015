ec15d79e35c6ef2bf2c05ec634e7dffb7eb5dbd9
tracer-ics2015 <tracer@njuics.org>
2015-12-08 09:49:24 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  09:49:24 up 47 min,  3 users,  load average: 0.16, 0.06, 0.06 60bcbc5454d77c4b8c20c2409cff935a9f1d5bfc

--- nemu/src/cpu/exec/data-mov/lgdt.c
+++ nemu/src/cpu/exec/data-mov/lgdt.c
@@ -4,12 +4,12 @@ int lgdt_rm2r_l(swaddr_t eip){
     printf("yes!\n");
     cpu._gdtr.limit = instr_fetch(eip, 2);
     cpu._gdtr.base  = instr_fetch(eip+2, 4);
-    return 4;
+    return 6;
 }
 
 int lgdt_rm2r_w(swaddr_t eip){
     Assert(0, "lgdt is not defined");
-    return 5;
+    return 6;
 }
 
 make_helper_v(lgdt_rm2r)
