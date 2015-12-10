677efebe0099a54706307bf6220964dbbe1d1f86
tracer-ics2015 <tracer@njuics.org>
2015-12-10 21:49:37 +0800

    >  compile kernel 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  21:49:37 up  2:57,  4 users,  load average: 0.00, 0.01, 0.05 3bd4a3b49aad723859db4d9490ee726b6bda91c8

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -63,6 +63,7 @@ void init_page(void) {
 	cr0.val = read_cr0();
 	cr0.paging = 1;
 	write_cr0(cr0.val);
+    panic("should not reach here");
 }
 
 /* GDT in the kernel's memory, whose virtual memory is greater than 0xC0000000. */
