6d9c358653cc5a12b13c37009ce6041bd59eea20
tracer-ics2015 <tracer@njuics.org>
2015-12-19 12:20:26 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  12:20:26 up  2:02,  5 users,  load average: 0.00, 0.01, 0.05 9e8231a2b6c1e41bdcf54b7de9ae6f38eff3112

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -46,7 +46,7 @@ uint32_t page_translate(lnaddr_t addr, size_t len){
         printf("eip: 0x%x\n", cpu.eip);
         printf("addr: 0x%x\n", addr);
         printf("pde_val: 0x%x\n", pde_val);
-        printf("pte_addr: 0x%x\n", pte_addr);
+        printf("pte_val: 0x%x\n", pte_val);
 
     if(!pte.present){
         Assert(0, "eip: 0x%x\nPageTable fault", cpu.eip);
