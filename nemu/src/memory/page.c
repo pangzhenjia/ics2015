e8f2534ee5ec2429a795a5c0e2f97529bf955112
tracer-ics2015 <tracer@njuics.org>
2015-12-19 10:29:51 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  10:29:51 up 11 min,  5 users,  load average: 0.00, 0.03, 0.05 921782d655bd363a9da45fbb334fbed3c24e1ab5

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -32,8 +32,10 @@ uint32_t page_translate(lnaddr_t addr, size_t len){
     uint32_t dir    = page_addr.dir;
     uint32_t page   = page_addr.page;
     uint32_t offset = page_addr.offset;
-    
-    uint32_t cr3_addr = cpu.cr3 & (~0xfff);
+   
+    printf("cr3: 0x%x\n", cpu.cr3);
+
+    uint32_t cr3_addr = cpu.CR3.page_directory_base;
 
     uint32_t pde_addr = cr3_addr | (dir << 2);
     uint32_t pde_val = hwaddr_read(pde_addr, 4);
