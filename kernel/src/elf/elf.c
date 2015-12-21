2b31592c4bcfdbdb6037e50de018c60f0f3583df
tracer-ics2015 <tracer@njuics.org>
2015-12-22 00:01:55 +0800

    >  compile kernel 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  00:01:55 up  1:38,  3 users,  load average: 0.00, 0.01, 0.05 d12ddd34821bf02a9a7b005d96bcf95ec8abcf03

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -47,7 +47,7 @@ uint32_t loader() {
 
             uint32_t hw_addr = mm_malloc(ph[i].p_vaddr, ph[i].p_memsz);
 
-            ramdisk_read((void *)hw_addr, ELF_OFFSET_IN_DISK + ph[i].p_offset, ph[i].p_filesz);
+            memcpy((void *)hw_addr, buf + ph[i].p_offset, ph[i].p_filesz);
             memset((void *)hw_addr + ph[i].p_filesz, 0, ph[i].p_memsz - ph[i].p_filesz);
 
 
