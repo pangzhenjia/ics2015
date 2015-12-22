8d7b8de7f662f972ed029eb7ba5365a75953ee38
tracer-ics2015 <tracer@njuics.org>
2015-12-22 21:11:28 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  21:11:28 up  1:16,  3 users,  load average: 0.00, 0.01, 0.05 bf514d33db727608dd42a43dc3504d71b8d943b9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -86,7 +86,7 @@ static int cmd_x(char *args){
     uint32_t value = cmd_p(expr), result = 0;
     int i = 0;
     for( ; i<num; i++){
-        result = swaddr_read(value,4, SR_DS);
+        result = hwaddr_read(value,4);
         printf("0x%08x\n", result);
         value += 4;
     }
