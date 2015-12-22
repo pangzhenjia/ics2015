7fe2d0eca4ba8a5aa41acd9d929c782551ce5795
tracer-ics2015 <tracer@njuics.org>
2015-12-22 21:03:03 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  21:03:03 up  1:07,  3 users,  load average: 0.00, 0.01, 0.05 47b8d906b64ebe82f0f05bddd69f1d59b1a633d

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,8 +1,8 @@
 #include "common.h"
 
 /* We don't use them now */
-//uint32_t dram_read(hwaddr_t, size_t);
-//void dram_write(hwaddr_t, size_t, uint32_t);
+uint32_t dram_read(hwaddr_t, size_t);
+void dram_write(hwaddr_t, size_t, uint32_t);
 
 uint32_t cache_read(hwaddr_t, size_t);
 void cache_write(hwaddr_t, size_t, uint32_t);
