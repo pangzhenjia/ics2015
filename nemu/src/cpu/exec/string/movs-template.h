f1e354ef9c2e8a05a8121734d4c8527cd584a7e4
tracer-ics2015 <tracer@njuics.org>
2015-12-25 22:56:44 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  22:56:44 up 11 min,  3 users,  load average: 0.00, 0.01, 0.01 3dd078e05071b7f86c1c005b1fc584a21cdca98f

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,8 +3,14 @@
 int concat(movs_, SUFFIX)(swaddr_t eip){
     DATA_TYPE val = MEM_R(reg_l(R_ESI));
     MEM_W(reg_l(R_EDI), val);
-    reg_l(R_ESI) += DATA_BYTE;
-    reg_l(R_EDI) += DATA_BYTE;
+    if(cpu.df == 0){
+        reg_l(R_ESI) += DATA_BYTE;
+        reg_l(R_EDI) += DATA_BYTE;
+    }
+    else{
+        reg_l(R_ESI) -= DATA_BYTE;
+        reg_l(R_EDI) -= DATA_BYTE;
+    }
     return 1;
 }
 
