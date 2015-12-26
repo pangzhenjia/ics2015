a4e7e0803006facb9fc155c6e7511d4594ffffaa
tracer-ics2015 <tracer@njuics.org>
2015-12-26 11:27:24 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  11:27:24 up 25 min,  5 users,  load average: 0.08, 0.04, 0.05 3012e248bfffc2c5cb654d3640eb01d9b70a8a6a

--- nemu/src/cpu/exec/string/std.c
+++
@@ -1,6 +0,0 @@
-#include "cpu/exec/helper.h"
-
-int std(swaddr_t eip){
-    cpu.df = 1;
-    return 1;
-}
--- nemu/src/cpu/exec/string/std.h
+++ nemu/src/cpu/exec/string/std.h
@@ -1,6 +1,9 @@
 #ifndef __STD_H__
 #define __STD_H__
 
-int std(swaddr_t eip);
+int std(swaddr_t eip){
+    cpu.df = 1;
+    return 1;
+}
 
 #endif
