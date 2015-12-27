a9cfda9039f4f2701d7606073668bf42f25d261e
tracer-ics2015 <tracer@njuics.org>
2015-12-27 19:17:39 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  19:17:39 up  2:44,  5 users,  load average: 0.00, 0.01, 0.05 ab28b740a51cb033e33fc101888ef438e620d84

--- nemu/src/cpu/exec/control/int.c
+++ nemu/src/cpu/exec/control/int.c
@@ -4,18 +4,21 @@
 void raise_intr(uint8_t );
 
 int int80(swaddr_t eip){
-
+   
     /* push eflags */
-   cpu.esp -= 4;
-   swaddr_write(cpu.esp, 4, SR_SS, cpu.eflags);
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, SR_SS, cpu.eflags);
 
-   /* push CS */
-   cpu.esp -= 4;
-   swaddr_write(cpu.esp, 4, SR_SS, cpu.Sreg[SR_CS].val);
+    cpu.If = 0;
+    cpu.tf = 0;
 
-   /* push eip */
-   cpu.esp -= 4;
-   swaddr_write(cpu.esp, 4, SR_SS, cpu.eip + 2);
+    /* push CS */
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, SR_SS, cpu.Sreg[SR_CS].val);
+
+    /* push eip */
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, SR_SS, cpu.eip + 2);
 
     print_asm("int80");
 
