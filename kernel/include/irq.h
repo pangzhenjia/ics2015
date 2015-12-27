615b7728efb23dc16efcf353395fcce12f402f07
tracer-ics2015 <tracer@njuics.org>
2015-12-27 20:07:52 +0800

    >  compile kernel 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  20:07:52 up  3:34,  5 users,  load average: 0.00, 0.01, 0.05 877f432c53d269865ff1dcfd78b3376141afc40f

--- kernel/include/irq.h
+++ kernel/include/irq.h
@@ -9,8 +9,24 @@
  */
 
 typedef struct TrapFrame {
-	uint32_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, old_esp, edi, ebp;
+	//uint32_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, old_esp, edi, ebp;
+
 	int32_t irq;
+    uint32_t error_code;
+
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t old_esp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+
+    uint32_t eip;
+    uint32_t cs;
+    uint32_t eflags;
+
 } TrapFrame;
 
 #endif
