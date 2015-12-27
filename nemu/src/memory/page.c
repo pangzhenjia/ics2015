735c9d5451a42fd8b457205edde4974f135ef3b6
tracer-ics2015 <tracer@njuics.org>
2015-12-27 20:56:28 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  20:56:28 up  4:23,  5 users,  load average: 0.17, 0.08, 0.06 901846bb426d9e711863690209187693db87caf

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -5,9 +5,9 @@ extern uint32_t tlb_translate(lnaddr_t addr);
 
 uint32_t page_translate(lnaddr_t addr, size_t len){
     /* if data cross the page boundary */
-    if ((addr % PAGE_SIZE) + len > PAGE_SIZE){
+    //if ((addr % PAGE_SIZE) + len > PAGE_SIZE){
         Assert(0, "eip: 0x%x\ndata cross the page boundary!\n", cpu.eip);
-    }
+    //}
 
     /* no page mode */
     if(!(cpu.CR0.pe == 1 && cpu.CR0.pg == 1)){
