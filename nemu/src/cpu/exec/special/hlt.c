58de69e25f54ff1c3485932901927ab64cf2b4ec
tracer-ics2015 <tracer@njuics.org>
2016-01-02 14:43:39 +0800

    >  compile NEMU 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  14:43:39 up  3:31,  3 users,  load average: 0.70, 0.22, 0.11 ec2889c744f36767582e5aca5aedc8c2503fe4

--- nemu/src/cpu/exec/special/hlt.c
+++ nemu/src/cpu/exec/special/hlt.c
@@ -13,7 +13,7 @@ int hlt(swaddr_t eip){
             uint32_t intr_no = i8259_query_intr();
             i8259_ack_intr();
             raise_intr(intr_no);
-            return 1;
+            return 0;
         }
     }
 
