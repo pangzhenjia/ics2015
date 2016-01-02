80a9e9138e77415e26607252e7c8b705700e0451
tracer-ics2015 <tracer@njuics.org>
2016-01-02 13:03:20 +0800

    >  compile kernel 131210022 zhenjiapang Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u4 (2015-09-19) i686 GNU/Linux  13:03:20 up  1:50,  3 users,  load average: 0.02, 0.07, 0.10 90dd654cfaaa168aadfe18d78bf1f2fb558650

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -9,7 +9,7 @@
 /* Use the function to get the start address of user page directory. */
 inline PDE* get_updir();
 
-static PTE vptable[(VMEM_ADDR + SCR_SIZE) / PAGE_SIZE + 1]  align_to_page;
+//static PTE vptable[(VMEM_ADDR + SCR_SIZE) / PAGE_SIZE + 1]  align_to_page;
 
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
@@ -17,7 +17,7 @@ void create_video_mapping() {
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
- 
+/* 
     PDE *pdir = va_to_pa(get_updir());
     PTE *ptable = va_to_pa((PTE *)vptable);
     uint32_t addr = VMEM_ADDR;
@@ -28,7 +28,6 @@ void create_video_mapping() {
     uint32_t pdir_idx = addr >> 22;
     uint32_t pt_idx = (addr << 10) >> 22;
 
-    /* copy the the previous pdir */
     if(pdir[pdir_idx].val != 0){
         PDE *pde = (PDE *)(pdir[pdir_idx].val & 0xfffff);
         memcpy(ptable, pde, nr_pt * sizeof(PTE));
@@ -41,7 +40,7 @@ void create_video_mapping() {
         ptable[pt_idx + i].val = make_pte(addr);
         addr += PAGE_SIZE;
     }
-
+*/
 }
 
 void video_mapping_write_test() {
